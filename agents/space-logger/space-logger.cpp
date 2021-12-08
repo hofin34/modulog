2d8761a12f351cc90ed1e8920fc569acc84772ad
Martin Hofbauer <martin.hofbauer@bringauto.com>
2021-12-08 10:31:06 +0100

    Free space logger uses config file

--- agents/space-logger/space-logger.cpp
+++ agents/space-logger/space-logger.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <asio.hpp>
-#include "../../communication/include/MessageSerializer.h"
 #include "../../communication/include/AgentClient.h"
 #include <thread>
-#include <sys/statvfs.h>
 #include <filesystem>
 
 
-int main(){
-    std::filesystem::space_info spaceInfo = std::filesystem::space("/");
+int main(int argc, char** argv){
+    nlohmann::json configJson = AgentClient::parseConfig(argv[0]);
+    if(!configJson.contains("id") || !configJson.contains("folderToMonitor")){
+        std::cerr << "You must specify id and folderToMonitor in config." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    int logInterval = 10;
+    if(configJson.contains("logInterval"))
+        logInterval = configJson["logInterval"];
+    std::filesystem::space_info spaceInfo = std::filesystem::space(configJson["folderToMonitor"]);
     auto ioContext = std::make_shared<asio::io_context>();
-    AgentClient agentClient(ioContext, false, "agent-space-logger" );
+    AgentClient agentClient(ioContext, false, configJson["id"]);
     agentClient.initClient();
     while(true){
         auto logMsg2 = std::make_shared<LogMessage>(LogMessage::LOG_MSG_TYPE::LOG, "availableSpaceMB", std::to_string(spaceInfo.available/1024));
         agentClient.sendLog(logMsg2);
         auto logMsg3 = std::make_shared<LogMessage>(LogMessage::LOG_MSG_TYPE::LOG, "freeSpaceMB", std::to_string(spaceInfo.free/1024));
         agentClient.sendLog(logMsg3);
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::seconds(logInterval));
     }
     return 0;
 }
