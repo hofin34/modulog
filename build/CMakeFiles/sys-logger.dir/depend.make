60ded101cc8f77f2b2617d87ca1d347d0dcfbc0a
Martin Hofbauer <martin.hofbauer@bringauto.com>
2021-12-21 15:29:11 +0100

    Refactor

--- build/CMakeFiles/sys-logger.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sys-logger.
-# This may be replaced when dependencies are built.
--- build/_deps/reproc++-build/reproc/CMakeFiles/reproc.dir/src/init.posix.c.o.d
+++
@@ -1,4 +0,0 @@
-_deps/reproc++-build/reproc/CMakeFiles/reproc.dir/src/init.posix.c.o: \
- /home/martin/School/bachelor-hofbauer/build/_deps/reproc++-src/reproc/src/init.posix.c \
- /usr/include/stdc-predef.h \
- /home/martin/School/bachelor-hofbauer/build/_deps/reproc++-src/reproc/src/init.h
