05849b7e72f04f32357f5385779ebb1efa9108bc
Martin Hofbauer <martin.hofbauer@bringauto.com>
2022-02-22 11:31:19 +0100

    Adding parallel test agents

--- test/smurf-test/agents/killed-agent/killed-agent.cpp
+++ test/smurf-test/agents/killed-agent/killed-agent.cpp
@@ -4,9 +4,10 @@ int main(){
     auto ioContext = std::make_shared<asio::io_context>();
     modulog::agent_client::AgentClient agentClient(ioContext, false, "killed-agent");
     agentClient.initClient();
-    for(int i = 0; i < 1000; i++){ // just sending a few logs
+    for(int i = 0; i < 5; i++){ // just sending a few logs
         auto msg = std::make_shared<modulog::communication::LogMessage>(modulog::communication::LogMessage::LOG_MSG_TYPE::LOG, "simpleLog", "simpleValue" + std::to_string(i));
         agentClient.sendLog(msg);
     }
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     return EXIT_FAILURE; // simulates agent kill
 }
---
+++ test/smurf-test/agents/permanent-log-agent/permanent-log-agent.cpp
@@ -0,0 +1,13 @@
+#include <modulog/agent_client/AgentClient.hpp>
+
+int main(){
+    auto ioContext = std::make_shared<asio::io_context>();
+    modulog::agent_client::AgentClient agentClient(ioContext, false, "simple-log-agent");
+    agentClient.initClient();
+    while(true){
+        auto msg = std::make_shared<modulog::communication::LogMessage>(modulog::communication::LogMessage::LOG_MSG_TYPE::LOG, "simpleLog", "simpleValue");
+        agentClient.sendLog(msg);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    agentClient.exitConnection();
+}
